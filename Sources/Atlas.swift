3f505dc44ab71ef635cefdfd0a9190b35fbfb019
Florian Heiber <flo@flohei.de>
2017-02-06 19:56:29 +0100

    adding availableCountriesAndFlag function

--- Sources/Atlas.swift
+++ Sources/Atlas.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public struct Country {
 	public let code: String
 
@@ -6,7 +8,7 @@ public struct Country {
 	}
 
 	public var emojiFlag: String {
-		return  code.unicodeScalars.map { String(regionalIndicatorSymbol(unicodeScalar: $0)!) }.joined()
+		return code.unicodeScalars.map { String(regionalIndicatorSymbol(unicodeScalar: $0)!) }.joined()
 	}
 
 	func regionalIndicatorSymbol(unicodeScalar: UnicodeScalar) -> UnicodeScalar? {
@@ -14,5 +16,16 @@ public struct Country {
 	    let regionalIndicatorSymbolA = UnicodeScalar("\u{1f1e6}")!
 		let distance = unicodeScalar.value - uppercaseA.value
 		return UnicodeScalar(regionalIndicatorSymbolA.value + distance)
+	}
+
+	func availableCountriesAndFlags() -> String {
+		var resultString = String()
+		for regionCode in Locale.isoRegionCodes {
+			let country = Locale.current.localizedString(forRegionCode: regionCode)
+			let flag = Country(code: regionCode).emojiFlag
+			resultString += "\(country): \(flag)\n"
+		}
+
+		return resultString
 	}
 }
